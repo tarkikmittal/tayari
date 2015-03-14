8559f71b2b679dbc51cd4a8d3e0b01189516bfc6
Tarkik Mittal <tarkikmittal@gmail.com>
2015-03-14 23:17:44 +0530

    Initial Commit

---
+++ justtotest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  justtotest
+//
+//  Created by TARKIK MITTAL on 14/03/15.
+//  Copyright (c) 2015 TARKIK MITTAL. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
